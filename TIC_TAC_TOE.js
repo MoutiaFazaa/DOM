44f7be4cf3714cf2e3b45c6fb4872bfc7c6b642a
MoutiaFazaa <fazaamouty@gmail.com>
2020-08-13 23:09:10 -0400

    tic tac toe done

--- TIC_TAC_TOE.js
+++ TIC_TAC_TOE.js
@@ -1,7 +1,25 @@
 //restart game buttom
-
+var restart = document.querySelector("#b");
 // grabs all the squares
-
+var squares = document.querySelector("td");
 //clear all the squares
-
+function clearBoard(){
+    for (var i=0; i<squares.length; i++ ){
+        squares[i].textContent="";
+    }
+}
+restart.addEventListener('click', clearBoard);
 //check the squares marker
+function changeMarker(){
+    if(this.textContent===""){
+        this.textContent="X";
+    } else if(this.textContent==="X"){
+        this.textContent="O";
+    }else{
+        this.textContent="";
+    }
+}
+for(var i=0; i<squares.length; i++){
+    squares[i].addEventListener("click",changeMarker);
+}
+// for loop to add event listeners to all squares
